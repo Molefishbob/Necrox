61ad9b5176bf1809505b999012a2119ac92f7160
Molefishbob <sami.sipari97@gmail.com>
2018-09-06 19:19:17 +0300

    The movement of 'rocks' is implemented.

---
+++ Necrox/Assets/Scripts/PlaySoundClip.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaySoundClip : MonoBehaviour {
+    public float pitchVariance;
+    private float lifeTime;
+    private float timer;
+
+    private void Update() {
+        timer += Time.deltaTime;
+        if(timer >= lifeTime) {
+            Destroy(gameObject);
+        }
+    }
+
+    public void PlayClip(AudioClip audioClip, float volume, bool usePitchVariance) {
+        AudioSource audioSource = GetComponent<AudioSource>();
+        audioSource.enabled = true;
+        audioSource.clip = audioClip;
+        audioSource.volume = volume;
+        if(usePitchVariance) audioSource.pitch = Random.Range(1 - pitchVariance, 1 + pitchVariance);
+        lifeTime = audioSource.clip.length + 1;
+        timer = 0;
+        audioSource.Play();
+    }
+}
---
+++ Necrox/Assets/Scripts/Rock.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rock : MonoBehaviour {
+
+	public int[] pos = new int[2];
+    private float speed = 0.018f;
+    private BoxCollider2D coll;
+
+	// Use this for initialization
+	void Start () {
+		coll = GetComponent<BoxCollider2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Moved) {
+            Vector3 wp = Camera.main.ScreenToWorldPoint(Input.GetTouch(0).position);
+            Vector2 touchPos = new Vector2(wp.x, wp.y);
+
+            if (coll == Physics2D.OverlapPoint(touchPos)) {
+                Vector2 touchDeltaPosition = Input.GetTouch(0).deltaPosition;
+
+                transform.Translate(touchDeltaPosition.x * speed, touchDeltaPosition.y * speed, 0);
+            }
+        }
+
+    }
+
+	public void Initialize(int[] pos) {
+		this.pos = pos;
+	}
+}
