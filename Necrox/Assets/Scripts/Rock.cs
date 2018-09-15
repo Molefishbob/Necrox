cf4367a03b69023c31fd6d0e273a6e1979ccf2e1
Molefishbob <sami.sipari97@gmail.com>
2018-09-15 15:43:38 +0300

    dsdad

--- Necrox/Assets/Scripts/Rock.cs
+++ Necrox/Assets/Scripts/Rock.cs
@@ -14,21 +14,11 @@ public class Rock : MonoBehaviour {
         }
 	
 	// Update is called once per frame
-	void FixedUpdate () {
+	void Update () {
         if (onMove) {
             transform.localPosition = new Vector3(0,transform.localPosition.y - 0.04f,0);
         }
     }
-/*
-    private void LateUpdate() {
-		Vector3 position = transform.localPosition;
-
-		position.x = (Mathf.Round(transform.parent.position.x * PPU) / PPU) - transform.parent.position.x; 
-		position.y = (Mathf.Round(transform.parent.position.y * PPU) / PPU) - transform.parent.position.y;
-
-		transform.localPosition = position;
-    }
-*/
 
 	public void Init(int x, int y) {
         int[] pos = new int[] {x,y};
