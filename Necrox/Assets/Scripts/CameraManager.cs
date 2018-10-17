59f79417f1d2e0af1d5f5c385335965be2abe1a6
Molefishbob <sami.sipari97@gmail.com>
2018-10-17 23:06:42 +0300

    Camera works now with 9:16 and 9:18 without area loss.

---
+++ Necrox/Assets/Scripts/CameraManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraManager : MonoBehaviour {
+
+	Camera _camera;
+
+	// Use this for initialization
+	void Start () {
+		_camera = gameObject.GetComponent<Camera>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (System.Math.Round(_camera.aspect,2) == (float) 9/18) {
+			_camera.orthographicSize  = 6.7f;
+		}
+		if (System.Math.Round(_camera.aspect,2) == System.Math.Round((float) 9/16,2)) {
+			_camera.orthographicSize  = 6;
+		}
+	}
+}
