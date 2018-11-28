06637bba96166ba72aef0e61915bea19561c4dfe
Anthony Vanoostendorp <36101168+AnthonyVanoo@users.noreply.github.com>
2018-11-28 11:52:09 +0200

    game state

--- Necrox/Assets/Scripts/States/GameStateBase.cs
+++ Necrox/Assets/Scripts/States/GameStateBase.cs
@@ -17,7 +17,19 @@ namespace TAMK.SpaceShooter.States
 		Level1,
 		Level2,
 		Level3,
-		Victory,
+        Level4,
+        Level5,
+        Level6,
+        Level7,
+        Level8,
+        Level9,
+        Level10,
+        Level11,
+        Level12,
+        Level13,
+        Level14,
+        Level15,
+        Victory,
 		GameOver
 	}
 
--- Necrox/Assets/Scripts/States/GameStateManager.cs
+++ Necrox/Assets/Scripts/States/GameStateManager.cs
@@ -72,8 +72,20 @@ namespace TAMK.SpaceShooter.States
 
 			_gameStates.Add( new LevelState( GameStateType.Level1, GameStateType.Level2, "Level1" ) );
 			_gameStates.Add( new LevelState( GameStateType.Level2, GameStateType.Level3, "Level2" ) );
-			_gameStates.Add( new LevelState( GameStateType.Level3, GameStateType.Level1, "Level3" ) );
-			_gameStates.Add( new GameOverState() );
+			_gameStates.Add( new LevelState( GameStateType.Level3, GameStateType.Level4, "Level3" ) );
+            _gameStates.Add( new LevelState( GameStateType.Level4, GameStateType.Level5, "Level4"));
+            _gameStates.Add(new LevelState(GameStateType.Level5, GameStateType.Level6, "Level5"));
+            _gameStates.Add(new LevelState(GameStateType.Level6, GameStateType.Level7, "Level6"));
+            _gameStates.Add(new LevelState(GameStateType.Level7, GameStateType.Level8, "Level7"));
+            _gameStates.Add(new LevelState(GameStateType.Level8, GameStateType.Level9, "Level8"));
+            _gameStates.Add(new LevelState(GameStateType.Level9, GameStateType.Level10, "Level9"));
+            _gameStates.Add(new LevelState(GameStateType.Level10, GameStateType.Level11, "Level10"));
+            _gameStates.Add(new LevelState(GameStateType.Level11, GameStateType.Level12, "Level11"));
+            _gameStates.Add(new LevelState(GameStateType.Level12, GameStateType.Level13, "Level12"));
+            _gameStates.Add(new LevelState(GameStateType.Level13, GameStateType.Level14, "Level13"));
+            _gameStates.Add(new LevelState(GameStateType.Level14, GameStateType.Level15, "Level14"));
+            _gameStates.Add(new LevelState(GameStateType.Level15, GameStateType.MainMenu, "Level15"));
+            _gameStates.Add( new GameOverState() );
 			_gameStates.Add( new VictoryState() );
 		}
 
