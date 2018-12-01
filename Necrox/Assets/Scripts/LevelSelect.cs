a80ff5b3ebaf58a846532c4b341fd3e366c8fc35
Anthony Vanoostendorp <anthony.vanoo@live.com>
2018-12-01 12:15:08 +0200

    can now switch between characters displayed for level select

--- Necrox/Assets/Scripts/LevelSelect.cs
+++ Necrox/Assets/Scripts/LevelSelect.cs
@@ -14,6 +14,14 @@ public class LevelSelect : MonoBehaviour {
     private string level;
     private int levelInt;
 
+    public Sprite elder;
+    public Sprite villagerMale;
+    public Sprite villagerFemale;
+    public Sprite minionRed;
+    public Sprite minionGreen;
+    public Sprite minionBlue;
+    public Sprite elderTree;
+
 
     void Start () {
         //select the latest level
@@ -33,16 +41,51 @@ public class LevelSelect : MonoBehaviour {
             levelInt += 1;
         }
         level = LevelFormat + levelInt;
-        Debug.Log(level);
         levelDisplay.text = "Level " + levelInt;
-
+        //need to change the character
+        switch (levelInt) {
+            case 1: enemyDisplayed.sprite = minionRed; break;
+            case 2: enemyDisplayed.sprite = villagerMale; break;
+            case 3: enemyDisplayed.sprite = minionGreen; break;
+            case 4: enemyDisplayed.sprite = villagerFemale; break;
+            case 5: enemyDisplayed.sprite = minionBlue; break;
+            case 6: enemyDisplayed.sprite = villagerMale; break;
+            case 7: enemyDisplayed.sprite = elder; break;
+            case 8: enemyDisplayed.sprite = villagerFemale; break;
+            case 9: enemyDisplayed.sprite = villagerMale; break;
+            case 10: enemyDisplayed.sprite = elder; break;
+            case 11: enemyDisplayed.sprite = minionGreen; break;
+            case 12: enemyDisplayed.sprite = minionRed; break;
+            case 13: enemyDisplayed.sprite = villagerMale; break;
+            case 14: enemyDisplayed.sprite = elderTree; break;
+            case 15: enemyDisplayed.sprite = elderTree; break;
+        }
+        
     }
 
     public void PreviousLevel() {
         if (levelInt > 1) {
             levelInt -= 1;
         }
+        level = LevelFormat + levelInt;
         levelDisplay.text = "Level " + levelInt;
+        switch (levelInt) {
+            case 1: enemyDisplayed.sprite = minionRed; break;
+            case 2: enemyDisplayed.sprite = villagerMale; break;
+            case 3: enemyDisplayed.sprite = minionGreen; break;
+            case 4: enemyDisplayed.sprite = villagerFemale; break;
+            case 5: enemyDisplayed.sprite = minionBlue; break;
+            case 6: enemyDisplayed.sprite = villagerMale; break;
+            case 7: enemyDisplayed.sprite = elder; break;
+            case 8: enemyDisplayed.sprite = villagerFemale; break;
+            case 9: enemyDisplayed.sprite = villagerMale; break;
+            case 10: enemyDisplayed.sprite = elder; break;
+            case 11: enemyDisplayed.sprite = minionGreen; break;
+            case 12: enemyDisplayed.sprite = minionRed; break;
+            case 13: enemyDisplayed.sprite = villagerMale; break;
+            case 14: enemyDisplayed.sprite = elderTree; break;
+            case 15: enemyDisplayed.sprite = elderTree; break;
+        }
     }
 
     public void StartLevel() {
