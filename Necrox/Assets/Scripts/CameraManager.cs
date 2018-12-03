d3c67a0ec4f6f91496a10d781e54529a3700e066
Sami Sipari <sami.sipari@cs.tamk.fi>
2018-12-04 00:21:51 +0200

    Fixed a bug with musicvolume resetting every level.

--- Necrox/Assets/Scripts/CameraManager.cs
+++ Necrox/Assets/Scripts/CameraManager.cs
@@ -11,21 +11,32 @@ public class CameraManager : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
+
 		_camera = gameObject.GetComponent<Camera>();
+		AudioSource audioSource = GetComponent<AudioSource>();
+		audioSource.volume = GameManager._musicVolume;
+		
 	}
 	
 	// Update is called once per frame
 	void Update () {
+
 		if (System.Math.Round(_camera.aspect,2) == (float) 9/18) {
+
 			_camera.orthographicSize  = 6.7f;
+
 		}
+
 		if (System.Math.Round(_camera.aspect,2) == System.Math.Round((float) 9/16,2)) {
 			_camera.orthographicSize  = 6;
+
 		}
+
 	}
 
     internal void PlaySound(AudioClip audioClip, float soundVolume, bool usePitchVariance)
     {
+
         Instantiate(_soundPlayer,transform.position,Quaternion.identity,transform).PlayClip(audioClip,soundVolume,usePitchVariance);
 
     }
