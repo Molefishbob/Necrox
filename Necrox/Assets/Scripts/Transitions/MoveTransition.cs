ecd7f57cebc064fa5b5f80f0ae0618cfe2bcf3e6
Sami Sipari <sami.sipari@cs.tamk.fi>
2018-12-17 22:36:50 +0200

    Added transitions to mainmenu. 2 generic transitions available.

---
+++ Necrox/Assets/Scripts/Transitions/MoveTransition.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveTransition : MonoBehaviour {
+
+	[SerializeField]
+	private float _time;
+	[SerializeField]
+	private float _endPosition;
+	[SerializeField]
+	private float _startPosition;
+    private bool _timeToStop;
+
+    // Use this for initialization
+    void Awake() {
+
+		transform.localPosition = new Vector2(_startPosition,transform.localPosition.y);
+		
+	}
+	
+	// Update is called once per frame
+	void Update() {
+
+		if (!_timeToStop) {
+
+			LeanTween.moveLocalX(gameObject,_endPosition,_time);
+			_timeToStop = true;
+
+		}
+	}
+
+	/// <summary>
+	/// This function is called when the behaviour becomes disabled or inactive.
+	/// </summary>
+	void OnDisable()
+	{
+		transform.localPosition = new Vector2(_startPosition,transform.localPosition.y);
+		_timeToStop = false;
+	}
+}
---
+++ Necrox/Assets/Scripts/Transitions/ScaleTransition.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScaleTransition : MonoBehaviour {
+
+	[SerializeField]
+	private float _time;
+    private bool _timeToStop;
+
+    // Use this for initialization
+    void Awake() {
+
+		transform.localScale = Vector2.zero;
+		
+	}
+	
+	// Update is called once per frame
+	void Update() {
+
+		if (!_timeToStop) {
+
+			LeanTween.scale(gameObject,Vector2.one,_time);
+			_timeToStop = true;
+
+		}
+	}
+
+	/// <summary>
+	/// This function is called when the behaviour becomes disabled or inactive.
+	/// </summary>
+	void OnDisable()
+	{
+		transform.localScale = Vector2.zero;
+		_timeToStop = false;
+	}
+}
